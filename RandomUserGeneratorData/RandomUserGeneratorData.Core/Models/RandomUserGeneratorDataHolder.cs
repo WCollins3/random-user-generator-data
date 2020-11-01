c3e9262b78117d040c808deb6d31688cab8a9c49
Bill Collins <wcollins113@gmail.com>
2020-10-31 22:55:46 -0400

    Implement RandomUserGeneratorLogic, create a constructor for RandomUserGeneratorDataHolder, and create IRandomUserRetriever interface

--- RandomUserGeneratorData/RandomUserGeneratorData.Core/Models/RandomUserGeneratorDataHolder.cs
+++ RandomUserGeneratorData/RandomUserGeneratorData.Core/Models/RandomUserGeneratorDataHolder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RandomUserGeneratorData.Core.Models
@@ -19,11 +20,26 @@ namespace RandomUserGeneratorData.Core.Models
         /// <summary>
         ///     Dictionary linking a letter to the number of <see cref="User"/>s who's first name starts with that letter.
         /// </summary>
-        public Dictionary<char, int> UsersByFirstNameFirstLetter { get; set; }
+        public Dictionary<string, int> UserCountsByFirstNameFirstLetter { get; set; }
 
         /// <summary>
         ///     Dictionary linking a letter to the number of <see cref="User"/>s who's last name starts with that letter.
         /// </summary>
-        public Dictionary<char, int> UsersByLirstNameFirstLetter { get; set; }
+        public Dictionary<string, int> UserCountsByLastNameFirstLetter { get; set; }
+
+        public RandomUserGeneratorDataHolder(IEnumerable<User> users)
+        {
+            UserCountsByGender = calculateUserCountsByTrait(users.Select(u => u.Gender));
+            UserCountsByCountry = calculateUserCountsByTrait(users.Select(u => u.Country));
+            UserCountsByFirstNameFirstLetter = calculateUserCountsByTrait(users.Select(u => u.FirstName.First().ToString()));
+            UserCountsByLastNameFirstLetter = calculateUserCountsByTrait(users.Select(u => u.LastName.First().ToString()));
+        }
+
+        private Dictionary<string, int> calculateUserCountsByTrait(IEnumerable<string> values)
+        {
+            return values.GroupBy(u => u)
+                .ToDictionary(g => g.Key, g => g.Count());
+        }
+
     }
 }
