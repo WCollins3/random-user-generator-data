912b1aab37d2045e598f023390f476e7d077331b
Bill Collins <wcollins113@gmail.com>
2020-10-31 21:51:00 -0400

    Turn WeatherForcastController into RandomUserGeneratorDataController and delete WeatherForcast.cs

---
+++ RandomUserGeneratorData/RandomUserGeneratorData/Controllers/RandomUserGeneratorDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RandomUserGeneratorData.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomUserGeneratorDataController : ControllerBase
+    {
+        private readonly ILogger<RandomUserGeneratorDataController> _logger;
+
+        public RandomUserGeneratorDataController(ILogger<RandomUserGeneratorDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("numberOfUsers/{numUsers:int}/data")]
+        public int Get(int numUsers)
+        {
+            return numUsers;
+        }
+    }
+}
