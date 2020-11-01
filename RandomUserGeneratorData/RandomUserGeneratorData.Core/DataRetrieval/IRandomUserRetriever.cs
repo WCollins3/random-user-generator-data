92722a2c31910e20b485aa3b13a5118ccedd8b6d
Bill Collins <wcollins113@gmail.com>
2020-10-31 22:55:46 -0400

    Implement RandomUserGeneratorLogic, create a constructor for RandomUserGeneratorDataHolder, and create IRandomUserRetriever interface

---
+++ RandomUserGeneratorData/RandomUserGeneratorData.Core/DataRetrieval/IRandomUserRetriever.cs
@@ -0,0 +1,17 @@
+﻿using RandomUserGeneratorData.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUserGeneratorData.Core.DataRetrieval
+{
+    public interface IRandomUserRetriever
+    {
+        /// <summary>
+        ///     Get <see cref="User"/>s from data source.
+        /// </summary>
+        /// <param name="numUsers">Number of <see cref="User"/>s to get.</param>
+        /// <returns></returns>
+        IEnumerable<User> GetUsers(int numUsers);
+    }
+}
---
+++ RandomUserGeneratorData/RandomUserGeneratorData.Logic/RandomUserGeneratorLogic.cs
@@ -0,0 +1,26 @@
+﻿using RandomUserGeneratorData.Core.DataRetrieval;
+using RandomUserGeneratorData.Core.Logic;
+using RandomUserGeneratorData.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUserGeneratorData.Logic
+{
+    public class RandomUserGeneratorLogic : IRandomUserGeneratorLogic
+    {
+        private IRandomUserRetriever _randomUserRetriever;
+
+        public RandomUserGeneratorLogic(IRandomUserRetriever randomUserRetriever)
+        {
+            _randomUserRetriever = randomUserRetriever;
+        }
+
+        /// <inheritdoc/>
+        public RandomUserGeneratorDataHolder GetRandomUserGeneratorDataHolder(int numUsers)
+        {
+            var users = _randomUserRetriever.GetUsers(numUsers);
+            return new RandomUserGeneratorDataHolder(users);
+        }
+    }
+}
