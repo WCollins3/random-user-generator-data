01491250932cfee2605ea05b1a40862456c76e6f
Bill Collins <wcollins113@gmail.com>
2020-10-31 22:08:48 -0400

    Add User and RandomUserGeneratorData models

---
+++ RandomUserGeneratorData/RandomUserGeneratorData.Core/Models/RandomUserGeneratorData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUserGeneratorData.Core.Models
+{
+    public class RandomUserGeneratorData
+    {
+        /// <summary>
+        ///     Dictionary linking a gender to the amount of <see cref="User"/>s that identify as that gender.
+        /// </summary>
+        public Dictionary<string, int> UserCountsByGender { get; set; }
+
+        /// <summary>
+        ///     Dictionary linking a country to the amount of people <see cref="User"/>s who live there.s
+        /// </summary>
+        public Dictionary<string, int> UserCountsByCountry { get; set; }
+
+        /// <summary>
+        ///     Dictionary linking a letter to the number of <see cref="User"/>s who's first name starts with that letter.
+        /// </summary>
+        public Dictionary<char, int> UsersByFirstNameFirstLetter { get; set; }
+
+        /// <summary>
+        ///     Dictionary linking a letter to the number of <see cref="User"/>s who's last name starts with that letter.
+        /// </summary>
+        public Dictionary<char, int> UsersByLirstNameFirstLetter { get; set; }
+    }
+}
